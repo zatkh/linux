338fb66739e8b7c23f3b2ba418bd7b367130e9d0
zatkh <zhtarkhani@gmail.com>
2020-08-20 09:20:13 +0100

    debug tests

--- tpt-netlink/netlinktest.c
+++ tpt-netlink/netlinktest.c
@@ -1,3 +1,7 @@
+#include <linux/module.h>
+#include <net/sock.h>
+#include <linux/netlink.h>
+#include <linux/skbuff.h>
 #include <net/genetlink.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -5,44 +9,15 @@
 #include <linux/tpt.h>
 #include <linux/mdom.h>
 #include <linux/sched.h>
+#include "nlink.h"
 
-/* attributes (variables): the index in this enum is used as a reference for the type,
- *             userspace application has to indicate the corresponding type
- *             the policy is used for security considerations
- */
+struct sock *nl_sk = NULL;
+struct nlmsghdr *nlh;
+struct sk_buff *skb_out;
 
-enum {
-	DOC_EXMPL_A_UNSPEC,
-	DOC_EXMPL_A_MSG,
-    __DOC_EXMPL_A_MAX,
-};
-#define DOC_EXMPL_A_MAX (__DOC_EXMPL_A_MAX - 1)
+int pid;
+int res;
 
-/* attribute policy: defines which attribute has which type (e.g int, char * etc)
- * possible values defined in net/netlink.h
- */
-static struct nla_policy doc_exmpl_genl_policy[DOC_EXMPL_A_MAX + 1] = {
-	[DOC_EXMPL_A_MSG] = { .type = NLA_NUL_STRING },
-};
-
-#define VERSION_NR 1
-/* family definition */
-static struct genl_family tpt_gnl_family = {
-	.hdrsize = 0,
-	.name = "CONTROL_EXMPL",        //the name of this family, used by userspace application
-	.version = VERSION_NR,                   //version number
-	.maxattr = DOC_EXMPL_A_MAX,
-};
-
-/* commands: enumeration of all commands (functions),
- * used by userspace application to identify command to be ececuted
- */
-enum {
-	DOC_EXMPL_C_UNSPEC,
-	DOC_EXMPL_C_ECHO,
-	__DOC_EXMPL_C_MAX,
-};
-#define DOC_EXMPL_C_MAX (__DOC_EXMPL_C_MAX - 1)
 
 int memdom_internal_functions(int memdom_op, long memdom_id1,
                                          long memdom_reqsize, unsigned long malloc_start, char* memdom_data, long smv_id,
@@ -58,7 +33,7 @@ int memdom_internal_functions(int memdom_op, long memdom_id1,
         rc = memdom_kill(memdom_id1, NULL);        
     }
     else if(memdom_op == 2){        
-        printk(KERN_CRIT "[%s] memdom_mmap_register(%ld)\n", __func__, memdom_id1);
+        printk(KERN_INFO "[%s] memdom_mmap_register(%ld)\n", __func__, memdom_id1);
         rc = memdom_mmap_register(memdom_id1);
     }
     else if(memdom_op == 3){
@@ -72,21 +47,22 @@ int memdom_internal_functions(int memdom_op, long memdom_id1,
     }
     else if(memdom_op == 4){      
         if(memdom_priv_op == 0){            
-            printk(KERN_CRIT "[%s] memdom_priv_get(%ld, %ld)\n", __func__, memdom_id1, smv_id);
+            printk(KERN_INFO "[%s] memdom_priv_get(%ld, %ld)\n", __func__, memdom_id1, smv_id);
             rc = memdom_priv_get(memdom_id1, smv_id);            
         }        
         else if(memdom_priv_op == 1){            
-            printk(KERN_CRIT "[%s] memdom_priv_add(%ld, %ld, %ld)\n", __func__, memdom_id1, smv_id, memdom_priv_value);
+            printk(KERN_INFO "[%s] memdom_priv_add(%ld, %ld, %ld)\n", __func__, memdom_id1, smv_id, memdom_priv_value);
             rc = memdom_priv_add(memdom_id1, smv_id, memdom_priv_value);            
         }        
         else if(memdom_priv_op == 2){            
-            printk(KERN_CRIT "[%s] memdom_priv_del(%ld, %ld, %ld)\n", __func__, memdom_id1, smv_id, memdom_priv_value);
+            printk(KERN_INFO "[%s] memdom_priv_del(%ld, %ld, %ld)\n", __func__, memdom_id1, smv_id, memdom_priv_value);
             rc = memdom_priv_del(memdom_id1, smv_id, memdom_priv_value);            
         }        
     }
 
     return rc;
 }
+
 
 int tpt_internal_functions(int tpt_op, long smv_id, int smv_domain_op,
                                           long memdom_id1, long memdom_id2){
@@ -116,6 +92,7 @@ int tpt_internal_functions(int tpt_op, long smv_id, int smv_domain_op,
     return rc;
 }
 
+
 int parse_message(char* message){
     char **buf;
     char *token;
@@ -135,11 +112,12 @@ int parse_message(char* message){
 
     int i = 0;
 
+    printk(KERN_INFO "parsing: %s\n", message);
+
     if(message == NULL)
         return 0;
     buf = &message;
 
-    printk(KERN_CRIT "parsing: %s\n", message);
 
     while( (token = strsep(buf, ",")) ){
 
@@ -164,7 +142,7 @@ int parse_message(char* message){
         /* token 2 */
         // decide operation
         if( message_type == 0 && memdom_op == -1){  // memdom
-            printk(KERN_CRIT "memdom token 2 (op): %s\n", token);
+            printk(KERN_INFO "memdom token 2 (op): %s\n", token);
 
             if( (strcmp(token, "create")) == 0 )
                 memdom_op = 0;
@@ -186,13 +164,13 @@ int parse_message(char* message){
             else if( (strcmp(token, "dumppgtable")) == 0) 
                 memdom_op = 9;
             else {
-                printk(KERN_CRIT "Error: received undefined memdom ops: %s\n", token);
+                printk(KERN_INFO "Error: received undefined memdom ops: %s\n", token);
                 return -1;
             }
             continue;
         }
         else if( message_type == 1 && tpt_op == -1){ // smv
-            printk(KERN_CRIT "smv token 2 (op): %s\n", token);
+            printk(KERN_INFO "smv token 2 (op): %s\n", token);
             if( (strcmp(token, "create")) == 0 )
                 tpt_op = 0;
             else if( (strcmp(token, "kill")) == 0 )
@@ -214,7 +192,7 @@ int parse_message(char* message){
             else if ((strcmp(token, "getsmvid")) == 0) 
                 tpt_op = 10; //get smv id
             else {
-                printk(KERN_CRIT "Error: received undefined smv ops: %s\n", token);
+                printk(KERN_INFO "Error: received undefined smv ops: %s\n", token);
                 return -1;
             }
             continue;
@@ -223,7 +201,7 @@ int parse_message(char* message){
         /* token 3 */
         // memdom: get memdom id
         if( message_type == 0 && (memdom_op >= 1 && memdom_op <=4) && memdom_id1 == -1 ){
-            printk(KERN_CRIT "memdom token 3 (memdom_id): %s\n", token);
+            printk(KERN_INFO "memdom token 3 (memdom_id): %s\n", token);
             if( kstrtol(token, 10, &memdom_id1) ){
                 return -1;
             }
@@ -232,17 +210,17 @@ int parse_message(char* message){
         // memdom: get query addr
         else if( message_type == 0 && memdom_op == 5 ){
             unsigned long address = 0;
-            printk(KERN_CRIT "memdom token 3 (addr): %s\n", token);
+            printk(KERN_INFO "memdom token 3 (addr): %s\n", token);
             if( kstrtoul(token, 10, &address) ){
                 return -1;
             }
-            printk(KERN_CRIT "addr: 0x%16lx\n", address);
+            printk(KERN_INFO "addr: 0x%16lx\n", address);
             return memdom_query_id(address);
         }
         
         // smv: get smv id
         else if( message_type == 1 && (tpt_op >= 1 || tpt_op <= 6) && smv_id == -1){
-            printk(KERN_CRIT "memdom token 3 (smv_id): %s\n", token);
+            printk(KERN_INFO "memdom token 3 (smv_id): %s\n", token);
             if( kstrtol(token, 10, &smv_id) ){
                 return -1;
             }
@@ -252,7 +230,7 @@ int parse_message(char* message){
         /* token 4*/
         // memdom
         if( message_type == 0 && (memdom_nbytes == -1 && memdom_data == NULL && smv_id == -1)){
-            printk(KERN_CRIT "memdom token 4: %s\n", token);
+            printk(KERN_INFO "memdom token 4: %s\n", token);
 
             // memdom allocate, get nbytes
             // deprecated, implemented in user space library
@@ -282,7 +260,7 @@ int parse_message(char* message){
             else if( (strcmp(token, "switch")) == 0)
                 smv_domain_op = 3;
             else {
-                printk(KERN_CRIT "Error: received undefined smv domain ops: %s\n", token);
+                printk(KERN_INFO "Error: received undefined smv domain ops: %s\n", token);
                 return -1;
             }
             continue;
@@ -293,7 +271,7 @@ int parse_message(char* message){
         if(message_type == 0){
             /* Get memdom privilege operations */
             if(memdom_op == 4 && memdom_priv_op == -1){
-                printk(KERN_CRIT "memdom token 5 (memdom_priv_op): %s\n", token);
+                printk(KERN_INFO "memdom token 5 (memdom_priv_op): %s\n", token);
 
                 if( (strcmp(token, "get")) == 0)
                     memdom_priv_op = 0;
@@ -304,16 +282,16 @@ int parse_message(char* message){
                 else if( (strcmp(token, "mod")) == 0)
                     memdom_priv_op = 3;
                 else{
-                    printk(KERN_CRIT "Error: received undefined memdom priv ops: %s\n", token);
+                    printk(KERN_INFO "Error: received undefined memdom priv ops: %s\n", token);
                     return -1;
                 }
                 continue;           
             }
             /* Get the starting address of malloced memory block*/
             else if (memdom_op == 2 && malloc_start == 0) {
-                printk(KERN_CRIT "memdom token 5 (starting address of malloced memory block): %s\n", token);
+                printk(KERN_INFO "memdom token 5 (starting address of malloced memory block): %s\n", token);
                 if(kstrtoul(token, 10, &malloc_start)){
-                    printk(KERN_CRIT "Error: failed to convert malloc addr: %lu\n", malloc_start);
+                    printk(KERN_INFO "Error: failed to convert malloc addr: %lu\n", malloc_start);
                     return -1;
                 }
                 continue;           
@@ -322,7 +300,7 @@ int parse_message(char* message){
         }
             // smv gets memory domain id
         else if(message_type == 1 && tpt_op == 3 && memdom_id1 == -1){
-            printk(KERN_CRIT "smv gets memdom_id1: %s\n", token);
+            printk(KERN_INFO "smv gets memdom_id1: %s\n", token);
             if( kstrtol(token, 10, &memdom_id1) )
                 return -1;
             continue;
@@ -332,17 +310,17 @@ int parse_message(char* message){
         /* token 6*/
         // memdom gets memdom privilege value 
         if( message_type == 0 && smv_id != -1 && memdom_priv_op != -1 && memdom_priv_value == -1){
-            printk(KERN_CRIT "memdom token 6 (memdom_priv_value): %s\n", token);
+            printk(KERN_INFO "memdom token 6 (memdom_priv_value): %s\n", token);
 
             if( kstrtol(token, 10, &memdom_priv_value) ){
-                printk(KERN_CRIT "Error: received undefined memdom priv value: %s\n", token);
+                printk(KERN_INFO "Error: received undefined memdom priv value: %s\n", token);
                 return -1;
             }
             continue;
         }
         // smv gets 2nd memory domain id
         else if( message_type == 1 && memdom_id1 != -1 && memdom_id2 == -1){
-            printk(KERN_CRIT "smv gets memdom_id2: %s\n", token);
+            printk(KERN_INFO "smv gets memdom_id2: %s\n", token);
             if( kstrtol(token, 10, &memdom_id2) )
                 return -1;
             continue;
@@ -399,119 +377,76 @@ int parse_message(char* message){
     }
 
     
-//    printk(KERN_DEBUG "[%s] unknown message: %s\n", __func__, message);
+   printk(KERN_DEBUG "[%s] unknown message: %s\n", __func__, message);
     
     return -1;
     
 }
 
-// the main msg handler: receives a message, parse it and sends the results back
-int main_handler(struct sk_buff *skb_2,  struct genl_info *info)
-{
-    struct nlattr *na;
-    struct sk_buff *skb;
-    int rc;
-	void *msg_head;
-	char * mydata = NULL;
-	int result;
+static void recv_msg(struct sk_buff *skb) {
+    printk(KERN_INFO "Entering: %s\n", __FUNCTION__);
+    nlh = (struct nlmsghdr*)skb->data;
     char buf[50];
-    
-    if (info == NULL)
-        goto out;
-    
-    /*for each attribute there is an index in info->attrs which points to a nlattr structure
-     *in this structure the data is given
-     */
-    na = info->attrs[DOC_EXMPL_A_MSG];
-    if (na) {
-		mydata = (char *)nla_data(na);
-		if (mydata == NULL)
-			printk(KERN_ERR "[smv_netlink.c] error while receiving data\n");
-    }
-	else
-		printk(KERN_CRIT "no info->attrs %i\n", DOC_EXMPL_A_MSG);
-    
-    /* Parse the received message here */
-    result = parse_message(mydata);
-    
-    /* send a message back*/
-    /* allocate some memory, since the size is not yet known use NLMSG_GOODSIZE*/
-    skb = genlmsg_new(NLMSG_GOODSIZE, GFP_KERNEL);
-	if (skb == NULL)
-		goto out;
-    
-	/* create the message headers */
+    int msg_size;
 
-    msg_head = genlmsg_put(skb, 0, info->snd_seq+1, &tpt_gnl_family, 0, DOC_EXMPL_C_ECHO);
-	if (msg_head == NULL) {
-		rc = -ENOMEM;
-		goto out;
-	}
-	/* add a DOC_EXMPL_A_MSG attribute (actual value to be sent) */
-    sprintf(buf, "%d", result);
-	rc = nla_put_string(skb, DOC_EXMPL_A_MSG, buf);
-	if (rc != 0)
-		goto out;
-	
-    /* finalize the message */
-	genlmsg_end(skb, msg_head);
+
+   int ret= parse_message((char*)nlmsg_data(nlh));
+   if(ret ==-1)
+      { printk(KERN_INFO "parse msg failed:%s\n",(char*)nlmsg_data(nlh));}
+
+    else
+    {
+        sprintf(buf, "%d", ret);
+        msg_size = strlen(buf);
+    }
     
-    /* send the message back */
-    rc = genlmsg_unicast(genl_info_net(info), skb, info->snd_portid);
-	if (rc != 0)
-		goto out;
-	return 0;
-    
-out:
-    printk("an error occured in main_handler:\n");
-    
+    printk(KERN_INFO "Netlink after parsing received msg payload:%s\n",(char*)nlmsg_data(nlh));
+
+    pid = nlh->nlmsg_pid;
+    printk(KERN_INFO "PID: %i\n", pid);
+
+    skb_out = nlmsg_new(msg_size, 0);
+
+    if(!skb_out)
+    {
+        printk(KERN_ERR "Failed to allocate new skb\n");
+
+        return;
+    }
+
+    nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);
+    NETLINK_CB(skb_out).dst_group = (1 << 3); /* mcast group */
+    strncpy(nlmsg_data(nlh), buf, msg_size);
+
+    res = nlmsg_unicast(nl_sk, skb_out, pid);
+
+    if(res<0) {
+        printk(KERN_INFO "Error while sending bak to user\n");
+    }
+}
+
+static int __init init(void) {
+    printk("[tpt] initializing: %s\n",__FUNCTION__);
+
+    struct netlink_kernel_cfg cfg = {
+        .input = recv_msg,
+    };
+
+    nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, &cfg);
+    if(!nl_sk)
+    {
+        printk(KERN_ALERT "Error creating socket.\n");
+        return -10;
+    }
+
     return 0;
 }
-/* commands: mapping between the command enumeration and the actual function*/
-static const struct genl_ops doc_exmpl_gnl_ops_echo[] = {
-    {
-    	.cmd = DOC_EXMPL_C_ECHO,
-  	    .flags = 0,
-    	.policy = doc_exmpl_genl_policy,
-	    .doit = main_handler,
-    },
-};
 
-static int __init kernel_comm_init(void)
-{
-	int rc;
-    printk(KERN_INFO "[tpt] initializing the module\n");
-
-    rc = genl_register_family(&tpt_gnl_family);
-	if (rc != 0){
-        printk("register ops: %i\n",rc);
-        genl_unregister_family(&tpt_gnl_family);
-		goto failure;
-    }    
-	return 0;
-	
-failure:
-    printk(KERN_CRIT "[tpt] error occured while inserting the netlink module\n");
-	return -1;	
+static void __exit exit(void) {
+    printk(KERN_INFO "exiting hello module\n");
+    netlink_kernel_release(nl_sk);
 }
 
-static void __exit kernel_comm_exit(void)
-{
-    int ret;
-    printk(KERN_INFO "[tpt] KERNEL COMMUNICATION MODULE EXIT\n");
+module_init(init); module_exit(exit);
 
-    /*unregister the family*/
-	ret = genl_unregister_family(&tpt_gnl_family);
-	if(ret !=0){
-        printk("unregister family %i\n",ret);
-    }
-}
-
-
-module_init(kernel_comm_init);
-module_exit(kernel_comm_exit);
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Main component for user space API to communicate with the kernel.");
- 
-
-
